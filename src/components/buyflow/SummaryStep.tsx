9a8b25c9009d02ce59d7aee1d064d1257f27b0f9
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-06-14 16:45:34 +0530

    showing in summary page

--- src/components/buyflow/SummaryStep.tsx
+++ src/components/buyflow/SummaryStep.tsx
@@ -5,14 +5,22 @@ interface SummaryStepProps {
   collectedData: {
     email: string;
     age: number;
+    firstName: string;
+    lastName: string;
   };
-}
+} 
 
 const SummaryStep: React.FC<SummaryStepProps> = (props) => {
   return (
     <div className="flex flex-col gap-4 w-full">
       <div className="flex flex-col gap-2 items-start">
         <div>Email: {props.collectedData.email}</div>
+        {props.collectedData.firstName !== "" && (
+          <div>First Name: {props.collectedData.firstName}</div>
+        )}
+        {props.collectedData.lastName !== "" && (
+          <div>Last Name: {props.collectedData.lastName}</div>
+        )}
         <div>Age: {props.collectedData.age}</div>
       </div>
       <Link className="btn btn-primary" href="/purchased=dev_ins">
