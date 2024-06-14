2185f17fe30298942c794205526cd8f67f3e5536
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-06-14 16:45:23 +0530

    Integrating the buyflow component with name component

--- src/components/buyflow/Buyflow.tsx
+++ src/components/buyflow/Buyflow.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AgeStep from "./AgeStep";
 import EmailStep from "./EmailStep";
 import SummaryStep from "./SummaryStep";
+import NameStep from "./NameStep";
 
 interface BuyflowProps {
   productId: ProductIds;
@@ -9,10 +10,12 @@ interface BuyflowProps {
 
 export enum ProductIds {
   devIns = "dev_ins",
+  desIns = "dev_des",
 }
 
 const PRODUCT_IDS_TO_NAMES = {
   [ProductIds.devIns]: "Developer Insurance",
+  [ProductIds.desIns]: "Designer Insurance",
 };
 
 const Buyflow: React.FC<BuyflowProps> = (props) => {
@@ -20,20 +23,49 @@ const Buyflow: React.FC<BuyflowProps> = (props) => {
   const [collectedData, updateData] = useState({
     email: "",
     age: 0,
+    firstName: "",
+    lastName: "",
   });
+
   const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value });
+    if (field == "name") {
+      if (value.lastName == "" || value.firstName == "") {
+        alert("Please enter details");
+        return;
+      }
+      updateData({
+        ...collectedData,
+        firstName: value.firstName,
+        lastName: value.lastName,
+      });
+    } else {
+      if (
+        (field !== "age" && String(value) == "") ||
+        (field == "age" && value <= 0)
+      ) {
+        alert("Please enter details");
+        return;
+      }
+      updateData({ ...collectedData, [field]: value });
+    }
     setStep(nextStep);
   };
+
   return (
     <div className="flex flex-col items-center">
       <h4 className="mb-6 text-2xl">
         Buying {PRODUCT_IDS_TO_NAMES[props.productId]}
       </h4>
       <div className="flex justify-center w-60">
-        {(currentStep === "email" && (
-          <EmailStep cb={getStepCallback("age")} />
-        )) ||
+        {(currentStep === "email" &&
+          (props.productId === ProductIds.devIns ? (
+            <EmailStep cb={getStepCallback("age")} />
+          ) : (
+            <EmailStep cb={getStepCallback("name")} />
+          ))) ||
+          (currentStep === "name" && props.productId === ProductIds.desIns && (
+            <NameStep cb={getStepCallback("age")} />
+          )) ||
           (currentStep === "age" && (
             <AgeStep cb={getStepCallback("summary")} />
           )) ||
